e3129c4b2a7230c02664c08a76861c6d41a57d76
David Hovanky <d.hovanky@axelmark.co.jp>
2015-01-20 18:04:49 +0900

    Routing to a single post page

--- server/fixture.js
+++ server/fixture.js
@@ -1,16 +1,19 @@
 if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
+    url: 'http://sachagreif.com/introducing-telescope/',
+    desc: 'Telescope'
   });
 
   Posts.insert({
     title: 'Meteor',
-    url: 'http://meteor.com'
+    url: 'http://meteor.com',
+    desc: 'Meteor stuff'
   });
 
   Posts.insert({
     title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
+    url: 'http://themeteorbook.com',
+    desc: 'The book'
   });
 }
